612a521fe1507f31fa16e03f7cf7ce827f098567
Your Name <quadekirby@gmail.com>
2026-05-05 21:07:42 -0400

    min mongo stuff

---
+++ app/components/reciepts.tsx
@@ -0,0 +1,16 @@
+import { OptionalId, Document } from "mongodb";
+import mongoclient from "../lib/mongodb";
+
+export async function uploadRecieptForm(req: OptionalId<Document>) {
+//   const session = await auth0.getSession();
+//   const user = session?.user;
+//   req.sub = user?.sub!;
+
+  const client = await mongoclient;
+  const db = client.db("cartlog");
+  const result = await db
+    .collection("reciepts")
+    .insertOne(req);
+  return result;
+
+}
---
+++ app/lib/mongodb.ts
@@ -0,0 +1,33 @@
+import {MongoClient} from 'mongodb';
+
+if (!process.env.MONGODB_URI) {
+  throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
+}
+
+const uri = process.env.MONGODB_URI;
+const options = {
+  appName: 'devrel.template.nextjs'
+};
+
+let mongodbclient: MongoClient;
+
+if (process.env.NODE_ENV === 'development') {
+  // In development mode, use a global variable so that the value
+  // is preserved across module reloads caused by HMR (Hot Module Replacement).
+  const globalWithMongo = global as typeof globalThis & {
+    _mongoClient?: MongoClient;
+  };
+
+  if (!globalWithMongo._mongoClient) {
+    globalWithMongo._mongoClient = new MongoClient(uri, options);
+  }
+  mongodbclient = globalWithMongo._mongoClient;
+} else {
+  // In production mode, it's best to not use a global variable.
+  mongodbclient = new MongoClient(uri, options);
+}
+
+// Export a module-scoped MongoClient. By doing this in a
+// separate module, the client can be shared across functions.
+
+export default mongodbclient;
